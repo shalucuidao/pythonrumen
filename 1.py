154957232313bfd6cadb672af6046791092453be
fengyunzaidushi <fengxiaoyang163@163.com>
2023-05-03 12:39:39 +0800

    add ceshi6

--- 1.py
+++ 1.py
@@ -13,7 +13,17 @@ def ceshi3():
 def ceshi4():
 	print('4')
 
+def ceshi5():
+	print('5')
+	print('55')
+	print('66dev1-1')
+	print('66dev-rebase')
+	print('77')
+	print('88')
+	print('1-1')
 
+def ceshi6():
+	print('4 + dev1')
 
 if __name__ == '__main__':
 	ceshi3()
